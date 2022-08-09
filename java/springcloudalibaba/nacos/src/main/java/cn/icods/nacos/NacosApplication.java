7f385040a246aba5482965de5c8eef1f0464b249
rex.zhao <rex.zhao@cloudwise.com>
2022-08-09 18:05:35 +0800

    nacos

---
+++ java/springcloudalibaba/nacos/src/main/java/cn/icods/nacos/NacosApplication.java
@@ -0,0 +1,12 @@
+package cn.icods.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+}
